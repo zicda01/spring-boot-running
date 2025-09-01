7385cc3fb28514703ba9fd6c6893e08db6345708
최수빈 <soobin1457@daum.net>
2025-09-01 17:03:48 +0900

    first commit

---
+++ chapter-6/learn-chapter.6-Redis/src/test/java/com/example/learn_chapter/__Redis/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_chapter.__Redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

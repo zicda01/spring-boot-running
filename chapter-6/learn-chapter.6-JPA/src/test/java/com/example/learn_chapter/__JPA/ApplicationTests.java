fb9518d29f96d5d4f9c977d982d8b8d268ac16d0
최수빈 <soobin1457@daum.net>
2025-09-04 17:47:54 +0900

    jpa 와 db 사용을 위한 new maven project

---
+++ chapter-6/learn-chapter.6-JPA/src/test/java/com/example/learn_chapter/__JPA/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_chapter.__JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
